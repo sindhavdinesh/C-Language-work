13684b444091c692a521f2d0c5adf485f3d62f40
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-01-26 22:02:49 +0530

    Update Pr6.c

--- C Language work/Circlet/Pr6.c
+++ C Language work/Circlet/Pr6.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
+    int i, j, n = 5;
 
-    for (i = 1; i <= 5; i++) {
+    for (i = 1; i <= n; i++) {
 
-        for (j = 1; j <= i; j++)
+        // left side increasing
+        for (j = 1; j <= i; j++) {
             printf("%d ", j);
+        }
 
-        for (j = i; j >= 1; j--)
+        // spaces
+        for (j = 1; j <= 2 * (n - i); j++) {
+            printf("  ");
+        }
+
+        // right side decreasing
+        for (j = i; j >= 1; j--) {
             printf("%d ", j);
+        }
 
         printf("\n");
     }
-
     return 0;
 }
 
+
 // output //
 
-1 1
-1 2 2 1
+1       1
+1 2   2 1
 1 2 3 3 2 1
 1 2 3 4 4 3 2 1
-1 2 3 4 5 5 4 3 2 1
+1 2 3 4 5 4 3 2 1
+
 
 --------------------------------
 Process exited after 0.1954 seconds with return value 0
+
 Press any key to continue . . .
