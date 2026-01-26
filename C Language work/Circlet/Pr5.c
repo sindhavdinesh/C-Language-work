3dfec6e19df2bbed18d7aa3ec9cc6621c1a44fe6
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-01-26 22:01:06 +0530

    Refactor to display pyramid pattern of numbers
    Updated the program to print a pyramid pattern of numbers with spaces.

--- C Language work/Circlet/Pr5.c
+++ C Language work/Circlet/Pr5.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 
 int main() {
-    int i, j;
+    int i, j, n = 5;
 
-    for (i = 5; i >= 1; i--) {
-        for (j = i; j <= 5; j++)
-            printf("%d ", j);
+    for (i = n; i >= 1; i--) {
 
-        for (j = 4; j >= i; j--)
+        // spaces
+        for (j = 1; j < i; j++) {
+            printf("  ");
+        }
+
+        // increasing numbers
+        for (j = i; j <= n; j++) {
             printf("%d ", j);
+        }
+
+        // decreasing numbers
+        for (j = n - 1; j >= i; j--) {
+            printf("%d ", j);
+        }
 
         printf("\n");
     }
-
     return 0;
 }
 
 // output //
 
-5
-4 5 4
-3 4 5 4 3
-2 3 4 5 4 3 2
+        5
+      4 5 4
+    3 4 5 4 3
+  3 3 4 5 4 3 2
 1 2 3 4 5 4 3 2 1
 
 --------------------------------
 Process exited after 0.2114 seconds with return value 0
+
 Press any key to continue . . .
