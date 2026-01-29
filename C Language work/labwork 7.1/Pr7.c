0191efe4c9300c67e0a4f2f6c3a2f4737e4c6dea
SINDHAV DINESH <sindhavdinesh93@gmail.com>
2026-01-29 21:09:55 +0530

    Add files via upload

---
+++ C Language work/labwork 7.1/Pr7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    char ch;
+
+    for(i = 0; i < 5; i++)
+    {
+        ch = 'A' + i;
+        for(j = 0; j <= i; j++)
+        {
+            printf("%c ", ch--);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
